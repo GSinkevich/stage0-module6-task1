16f89c6aa0020b4fc84d73dc7d21e749328ca4e7
DESKTOP-GV962H6\sinke <sinkevich.work@gmail.com>
2022-12-24 22:58:40 +0300

    done

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,21 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    public String color;
+    public int numberOfPaws;
+    public boolean hasFur;
 
+    public Animal(String color, int numberOfPaws,boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String Res = "";
+        String paws = "";
+        Res = hasFur ? "a": "no";
+        paws = numberOfPaws > 1 ? "paws": "paw";
+        return  "This animal is mostly " +color +". It has " +numberOfPaws+" "+ paws +" and " +Res+" fur.";
+    }
 }
